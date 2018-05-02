91d155af79c1ed8c463c531989853af7acf588ed
Yanniel Fandiño García <33382521+YannFandino@users.noreply.github.com>
2018-05-02 21:49:41 +0200

    Modificacion BD
    Se ha modifica la base de datos, se cambia el motor a InnoDB.

--- db/nc_db.sql
+++ db/nc_db.sql
@@ -10,8 +10,15 @@ CREATE TABLE IF NOT EXISTS table_users (
 	email VARCHAR(100) NOT NULL,
 	password VARCHAR(20) NOT NULL,
 	nombre VARCHAR(50) NOT NULL,
-	CONSTRAINT pk_user PRIMARY KEY (id_user)
-);
+	CONSTRAINT pk_user PRIMARY KEY (id_user),
+	UNIQUE (email)
+) ENGINE InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS table_categorias (
+	id_categoria INT NOT NULL AUTO_INCREMENT,
+	descripcipm VARCHAR(25) NOT NULL,
+	CONSTRAINT pk_categorias PRIMARY KEY (id_categoria)
+) ENGINE InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS table_rss (
 	id_rss INT NOT NULL AUTO_INCREMENT,
@@ -21,7 +28,7 @@ CREATE TABLE IF NOT EXISTS table_rss (
 	xml VARCHAR(100) NOT NULL,
 	CONSTRAINT pk_rss PRIMARY KEY (id_rss),
 	CONSTRAINT fk_rss_categoria FOREIGN KEY (rss2categoria) REFERENCES table_categorias (id_categoria)
-);
+) ENGINE InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS table_user_feed (
 	id_feed INT NOT NULL AUTO_INCREMENT,
@@ -30,10 +37,4 @@ CREATE TABLE IF NOT EXISTS table_user_feed (
 	CONSTRAINT pk_tuserfeed PRIMARY KEY (id_feed),
 	CONSTRAINT fk_userfeed_user FOREIGN KEY (userfeed2user) REFERENCES table_users(id_user),
 	CONSTRAINT fk_userfeed_rss FOREIGN KEY (userfeed2rss) REFERENCES table_rss(id_rss)
-);
-
-CREATE TABLE IF NOT EXISTS table_categorias (
-	id_categoria INT NOT NULL AUTO_INCREMENT,
-	descripcipm VARCHAR(25) NOT NULL
-	CONSTRAINT pk_categorias PRIMARY KEY (id_categoria)
-);
+) ENGINE InnoDB DEFAULT CHARSET=utf8;
